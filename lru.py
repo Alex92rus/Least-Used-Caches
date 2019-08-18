dc86d9afe133bf417e995bf6bb05c8baa8cfe76d
Alex92rus <aleksandar.rusinov@gmail.com>
2019-08-18 07:11:57 +0100

    Initial implementation of Least Recently Used Cache in Python.
    WARN: static cache size!

---
+++ lru.py
@@ -0,0 +1,77 @@
+
+
+class Entry:
+
+    def __init__(self, key, value):
+        self.left = None
+        self.right = None
+        self.key = key
+        self.value = value
+
+
+class Lru:
+
+    def __init__(self):
+        self.start = None
+        self.end = None
+        self.entryMap = {}
+        self.lruSize = 4
+
+    def getEntry(self, key):
+        if key in self.entryMap:
+            entry = self.entryMap[key]
+            self._removeEntry(entry)
+            self._addAtStart(entry)
+            return entry
+        else:
+            return -1
+
+    def addEntry(self, key, value):
+        if key in self.entryMap:
+            entry = self.entryMap[key]
+            entry.value = value
+            self._removeEntry(entry)
+            self._addAtStart(entry)
+        else:
+            newEntry = Entry(key, value)
+            if len(self.entryMap) == self.lruSize:
+                self._removeEntry(self.end)
+            self.entryMap[key] = newEntry
+            self._addAtStart(newEntry)
+
+    def _removeEntry(self, entry):
+
+        if entry.right is not None:
+            entry.right.left = entry.left
+        else:
+            self.end = entry.left
+
+        if entry.left is not None:
+            entry.left.right = entry.right
+        else:
+            self.start = entry.right
+
+    def _addAtStart(self, entry):
+
+        if self.start is not None:
+            self.start.left = entry
+
+        entry.right = self.start
+        self.start = entry
+
+        if self.end is None:
+            self.end = self.start
+
+
+    def printDoublyLinkedList(self):
+        iterDL = self.start
+        while iterDL is not None:
+            print("({}->{})=>".format(iterDL.key, iterDL.value), end="")
+            iterDL = iterDL.right
+        print("None")
+
+if __name__ == '__main__':
+    lru = Lru()
+    lru.addEntry(4, 16)
+    lru.addEntry(3, 9)
+    lru.printDoublyLinkedList()
